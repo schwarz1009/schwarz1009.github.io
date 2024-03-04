3c9ddd73f9016dd9b970662b7bdbf2ec3a695af5
Assy <schwarz1009@gmail.com>
2024-03-04 17:24:12 +0900

    日記を更新。化学について編集。TeXや工学や農業について追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -179,6 +179,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.01.html">2024-03-01</a></li>\
 <li><a href="2024.03.02.html">2024-03-02</a></li>\
 <li><a href="2024.03.03.html">2024-03-03</a></li>\
+<li><a href="2024.03.04.html">2024-03-04</a></li>\
 </ul>\
 </div>\
 </div>\
