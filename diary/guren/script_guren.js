59ac30e06396d08e2153143829e49fc4cdba7790
Assy <schwarz1009@gmail.com>
2024-03-05 15:17:22 +0900

    日記を更新。BeOSについて編集。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -180,6 +180,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.02.html">2024-03-02</a></li>\
 <li><a href="2024.03.03.html">2024-03-03</a></li>\
 <li><a href="2024.03.04.html">2024-03-04</a></li>\
+<li><a href="2024.03.05.html">2024-03-05</a></li>\
 </ul>\
 </div>\
 </div>\
