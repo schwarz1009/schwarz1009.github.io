945d094c1e4686ebd5ac54231421329c0e742e95
Assy <schwarz1009@gmail.com>
2024-03-15 04:55:32 +0900

    日記を更新。アニメについて追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -188,6 +188,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.12.html">2024-03-12</a></li>\
 <li><a href="2024.03.13.html">2024-03-13</a></li>\
 <li><a href="2024.03.14.html">2024-03-14</a></li>\
+<li><a href="2024.03.15.html">2024-03-15</a></li>\
 </ul>\
 </div>\
 </div>\
