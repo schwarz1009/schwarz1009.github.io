617f4a3f577fd1a4fe4a9a758997753f11c2b229
Assy <schwarz1009@gmail.com>
2024-03-18 15:35:55 +0900

    日記を更新。ピアノの画像を差し替え。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -191,6 +191,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.15.html">2024-03-15</a></li>\
 <li><a href="2024.03.16.html">2024-03-16</a></li>\
 <li><a href="2024.03.17.html">2024-03-17</a></li>\
+<li><a href="2024.03.18.html">2024-03-18</a></li>\
 </ul>\
 </div>\
 </div>\
