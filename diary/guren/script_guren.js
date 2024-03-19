481f54496ccd4bde9c2b7c5d98745791d6177626
Assy <schwarz1009@gmail.com>
2024-03-19 21:35:47 +0900

    日記を更新。Linuxについて追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -192,6 +192,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.16.html">2024-03-16</a></li>\
 <li><a href="2024.03.17.html">2024-03-17</a></li>\
 <li><a href="2024.03.18.html">2024-03-18</a></li>\
+<li><a href="2024.03.19.html">2024-03-19</a></li>\
 </ul>\
 </div>\
 </div>\
