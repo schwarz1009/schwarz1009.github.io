23938e12511e925024c022815d43906fb625a142
Assy <schwarz1009@gmail.com>
2024-03-20 17:33:57 +0900

    日記を更新。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -193,6 +193,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.17.html">2024-03-17</a></li>\
 <li><a href="2024.03.18.html">2024-03-18</a></li>\
 <li><a href="2024.03.19.html">2024-03-19</a></li>\
+<li><a href="2024.03.20.html">2024-03-20</a></li>\
 </ul>\
 </div>\
 </div>\
