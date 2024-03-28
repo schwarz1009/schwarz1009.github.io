8215cd161e79951a9260f5615245905abb7c1d1f
Assy <schwarz1009@gmail.com>
2024-03-28 15:41:30 +0900

    日記を更新。アフリカとインドについて追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -201,6 +201,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.03.25.html">2024-03-25</a></li>\
 <li><a href="2024.03.26.html">2024-03-26</a></li>\
 <li><a href="2024.03.27.html">2024-03-27</a></li>\
+<li><a href="2024.03.28.html">2024-03-28</a></li>\
 </ul>\
 </div>\
 </div>\
