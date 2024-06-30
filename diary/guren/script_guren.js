382668ae11c064503183c18d47081206224583cf
Assy <schwarz1009@gmail.com>
2024-06-30 17:04:25 +0900

    日記を更新。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -304,6 +304,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.06.26.html">2024-06-26</a></li>\
 <li><a href="2024.06.27.html">2024-06-27</a></li>\
 <li><a href="2024.06.28.html">2024-06-28</a></li>\
+<li><a href="2024.06.30.html">2024-06-30</a></li>\
 </ul>\
 </div>\
 </div>\
