c72d354c21c42413479a96f07ea9262d02e77826
Assy <schwarz1009@gmail.com>
2024-09-03 15:49:35 +0900

    日記を更新。さまざまな内容を追記。日記へのリンクを追加。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -391,6 +391,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <ul>\
 <li><a href="2024.09.01.html">2024-09-01</a></li>\
 <li><a href="2024.09.02.html">2024-09-02</a></li>\
+<li><a href="2024.09.03.html">2024-09-03</a></li>\
 </ul>\
 </div>\
 </div>\
