010346ca7a3b5fb265a1e1956703c5c930467860
Assy <schwarz1009@gmail.com>
2024-12-01 19:34:18 +0900

    first commit

---
+++ js/script_smartmenus_backup.js
@@ -0,0 +1,202 @@
+document.write("<link href='/smartmenus/css/sm-core-css.css' rel='stylesheet' type='text/css' />");
+document.write("<link href='/smartmenus/css/sm-mint/sm-mint.css' rel='stylesheet' type='text/css' />");
+document.write("<script type='text/javascript' src='/smartmenus/jquery.smartmenus.min.js'></script>");
+
+document.write('\
+<!--<div class="titleimage"><a href="/"><img src="/title19.png" alt="タイトルイメージ"></a></div>-->\
+  <ul id="main-menu" class="sm sm-mint">\
+    <li><a href="#">ホーム</a>\
+      <ul>\
+        <li><a href="/">トップページ</a></li>\
+        <li><a href="/changes.html">過去の変更履歴</a></li>\
+        <li><a href="/sitemap.html">サイトマップ</a></li>\
+<!--        <li><a href="/singlepage.html">シングルページ版</a></li>-->\
+        <li><a href="/subpages/blog.html">ブログ・SNS</a></li>\
+        <!--<li><a href="/subpages/templates.html">テンプレート</a></li>-->\
+        <li><a href="/subpages/oldhomepage.html">昔のホームページ</a></li>\
+      </ul>\
+    </li>\
+<!--    <li><a href="#"></a>\
+      <ul>\
+        <li><a href="/subpages/news.html">最新情報</a></li>\
+      </ul>\
+    </li>-->\
+    <li><a href="#">自己紹介</a>\
+      <ul>\
+        <li><a href="/subpages/profile.html">自己紹介・注意書き</a></li>\
+        <li><a href="/myhistory.html">略歴</a></li>\
+        <li><a href="/profile.html">自己紹介２</a></li>\
+        <li><a href="/favorites_and_places.html">お気に入りと場所</a></li>\
+        <li><a href="/english_text.html">English Texts</a></li>\
+      </ul>\
+    </li>\
+    <li><a href="#">小説・詩集</a>\
+      <ul>\
+        <li><a href="/novels/index.html">小説</a></li>\
+        <li><a href="/novels/poem.html">詩集</a></li>\
+        <li><a href="/subpages/texts.html">散文集</a></li>\
+      </ul>\
+    </li>\
+    <li><a href="#">日記</a>\
+      <ul>\
+        <li><a href="/blog_frey/index.html">わたしの名はフレイ（最新の日記）</a></li>\
+        <li><a href="/subpages/diary.html">日記</a></li>\
+        <li><a href="/blog/index.html">日記一覧（2012 ～ 2016-06）</a></li>\
+        <li><a href="#">月別一覧</a>\
+          <ul>\
+            <li><a href="/blog/list/index.html">月別一覧</a></li>\
+            <li><a href="#">2012</a>\
+              <ul>\
+<li><a href="/blog/list/2012.firsthalf.html">2012年前半期</a></li>\
+<li><a href="/blog/list/2012.07.html">2012-07</a></li>\
+<li><a href="/blog/list/2012.08.html">2012-08</a></li>\
+<li><a href="/blog/list/2012.09.html">2012-09</a></li>\
+<li><a href="/blog/list/2012.10.html">2012-10</a></li>\
+<li><a href="/blog/list/2012.11.html">2012-11</a></li>\
+<li><a href="/blog/list/2012.12.html">2012-12</a></li>\
+              </ul>\
+            </li>\
+            <li><a href="#">2013</a>\
+              <ul>\
+<li><a href="/blog/list/2013.01.html">2013-01</a></li>\
+<li><a href="/blog/list/2013.02.html">2013-02</a></li>\
+<li><a href="/blog/list/2013.03.html">2013-03</a></li>\
+<li><a href="/blog/list/2013.04.html">2013-04</a></li>\
+<li><a href="/blog/list/2013.05.html">2013-05</a></li>\
+<li><a href="/blog/list/2013.06.html">2013-06</a></li>\
+<li><a href="/blog/list/2013.07.html">2013-07</a></li>\
+<li><a href="/blog/list/2013.08.html">2013-08</a></li>\
+<li><a href="/blog/list/2013.09.html">2013-09</a></li>\
+<li><a href="/blog/list/2013.10.html">2013-10</a></li>\
+<li><a href="/blog/list/2013.11.html">2013-11</a></li>\
+<li><a href="/blog/list/2013.12.html">2013-12</a></li>\
+              </ul>\
+            </li>\
+            <li><a href="#">2014</a>\
+              <ul>\
+<li><a href="/blog/list/2014.05.html">2014-05</a></li>\
+<li><a href="/blog/list/2014.06.html">2014-06</a></li>\
+<li><a href="/blog/list/2014.07.html">2014-07</a></li>\
+<li><a href="/blog/list/2014.08.html">2014-08</a></li>\
+<li><a href="/blog/list/2014.09.html">2014-09</a></li>\
+<li><a href="/blog/list/2014.10.html">2014-10</a></li>\
+<li><a href="/blog/list/2014.11.html">2014-11</a></li>\
+<li><a href="/blog/list/2014.12.html">2014-12</a></li>\
+              </ul>\
+            </li>\
+            <li><a href="#">2015</a>\
+              <ul>\
+<li><a href="/blog/list/2015.01.html">2015-01</a></li>\
+<li><a href="/blog/list/2015.02.html">2015-02</a></li>\
+<li><a href="/blog/list/2015.03.html">2015-03</a></li>\
+<li><a href="/blog/list/2015.04.html">2015-04</a></li>\
+<li><a href="/blog/list/2015.05.html">2015-05</a></li>\
+<li><a href="/blog/list/2015.06.html">2015-06</a></li>\
+<li><a href="/blog/list/2015.07.html">2015-07</a></li>\
+<li><a href="/blog/list/2015.08.html">2015-08</a></li>\
+<li><a href="/blog/list/2015.09.html">2015-09</a></li>\
+<li><a href="/blog/list/2015.10.html">2015-10</a></li>\
+<li><a href="/blog/list/2015.11.html">2015-11</a></li>\
+<li><a href="/blog/list/2015.12.html">2015-12</a></li>\
+              </ul>\
+            </li>\
+            <li><a href="#">2016</a>\
+              <ul>\
+<li><a href="/blog/list/2016.01.html">2016-01</a></li>\
+<li><a href="/blog/list/2016.02.html">2016-02</a></li>\
+<li><a href="/blog/list/2016.03.html">2016-03</a></li>\
+<li><a href="/blog/list/2016.04.html">2016-04</a></li>\
+<li><a href="/blog/list/2016.05.html">2016-05</a></li>\
+<li><a href="/blog/list/2016.06.html">2016-06</a></li>\
+              </ul>\
+            </ul>\
+          </li>\
+        </li>\
+        <li><a href="#">最近の日記</a>\
+          <ul>\
+            <li><a href="/diary/hatenablog/index.html">はてなブログ（16-06 ～ 09）</a></li>\
+            <li><a href="/diary/ninjablog/index.html">忍者ブログ（16-09 ～ 10）</a></li>\
+            <li><a href="/diary/new_diary/index.html">新・日記（16-10）</a></li>\
+            <li><a href="/diary/ninjablog2/index.html">忍者ブログ２（16-10 ～ 11）</a></li>\
+            <li><a href="/diary/twitter/index.html">ツイッター（16-11 ～ 17-01）</a></li>\
+            <li><a href="/diary/ninjablog3/index.html">忍者ブログ３（17-01）</a></li>\
+            <li><a href="/diary/hatenablog2/index.html">はてなブログ２（17-01 ～ 05）</a></li>\
+            <li><a href="/diary/tumblr/index.html">Tumblr（17-05）</a></li>\
+            <li><a href="/diary/twitter2/index.html">ツイッター２（17-06）</a></li>\
+            <li><a href="/diary/new_diary2/index.html">新・日記２（17-06 ～ 20-04）</a></li>\
+          </ul>\
+        </li>\
+        <li><a href="#">その他の日記</a>\
+          <ul>\
+            <li><a href="/diary/notebooks/index.html">ノートに書いた日記</a></li>\
+            <li><a href="/diary/2009/index.html">2009年の日記</a></li>\
+          </ul>\
+        </li>\
+      </ul>\
+    </li>\
+        <!--<li><a href="#">政党</a>\
+        <ul>\
+          <li><a href="/llcp/index.html">労働地域党</a></li>\
+          <li><a href="/fpp/index.html">自由平和党</a></li>\
+        </ul>\
+        </li>-->\
+<!--<li><a href="#">哲学</a>\
+<ul>\
+    <li><a href="/philosophy/index.html">哲学</a></li>\
+</ul>\
+</li>-->\
+<!--<li><a href="#">世界観</a>\
+<ul>-->\
+<li><a href="#">IT・情報</a>\
+<ul>\
+            <li><a href="#">Linux</a><ul><li><a href="/links2/linux/index.html">Linux</a></li><li><a href="/links2/linux/index_desktop.html">Linuxデスクトップ</a></li><li><a href="/links2/linux/index_apps.html">Linuxアプリケーション</a></li></ul></li>\
+            <li><a href="#">Windows/PC</a><ul><li><a href="/links2/windows_pc/index.html">Windows/PC</a></li><li><a href="/links2/windows_pc/index_msoffice.html">MS-Office</a></li><li><a href="/links2/windows_pc/index_softs.html">Windowsソフトウェア</a></li></ul></li>\
+            <li><a href="#">UNIX</a><ul><li><a href="/links2/unix/index.html">UNIX</a></li><li><a href="/links2/unix/index_bsd.html">BSD</a></li></ul></li>\
+            <li><a href="#">OS</a><ul><li><a href="/links2/os/index.html">OS</a></li><li><a href="/links2/os/index_mac.html">Mac</a></li><li><a href="/links2/os/index_mobile.html">モバイル</a></li></ul></li>\
+<!--            <li><a href="/links2/software/index.html">ソフトウェア</a></li>-->\
+            <li><a href="/links2/hardware/index.html">ハードウェア</a></li>\
+            <li><a href="#">プログラミング言語</a><ul><li><a href="/links2/prog_lang/index.html">プログラミング言語</a></li><li><a href="/links2/prog_lang/index_skill.html">プログラミングスキル</a></li><li><a href="/links2/prog_lang/index_c.html">C/C++</a></li><li><a href="/links2/prog_lang/index_java.html">Java</a></li><li><a href="/links2/prog_lang/index_ll.html">LL系言語</a></li><li><a href="/links2/prog_lang/index_javascript.html">JavaScript</a></li><li><a href="/links2/prog_lang/index_windowsprog.html">Windowsプログラミング</a></li><li><a href="/links2/prog_lang/index_it_society.html">IT社会</a></li></ul></li>\
+            <li><a href="/links2/programs/index.html">自作プログラム</a></li>\
+            <li><a href="#">ネットワーク</a><ul><li><a href="/links2/network/index.html">ネットワーク</a></li><li><a href="/links2/network/index_webserver.html">Webサーバー</a></li><li><a href="/links2/network/index_fileserver.html">ファイルサーバー</a></li></ul></li>\
+            <li><a href="/links2/db/index.html">データベース</a></li>\
+            <li><a href="/links2/services/index.html">Webサービス</a></li>\
+</ul>\
+</li>\
+    <li><a href="#">自然</a>\
+      <ul>\
+            <li><a href="/links2/math/index.html">数学</a></li>\
+            <li><a href="/links2/natural/index.html">自然科学</a></li>\
+            <li><a href="/links2/tech/index.html">工学・機械技術</a></li>\
+            <li><a href="/links2/farm/index.html">農業・牧畜</a></li>\
+      </ul>\
+    </li>\
+    <li><a href="#">人文・社会</a>\
+      <ul>\
+            <li><a href="/links2/philosophy/index.html">哲学</a></li>\
+            <li><a href="#">社会科学</a><ul><li><a href="/links2/social/index.html">社会科学</a></li><li><a href="/links2/social/index_party.html">政党</a></li></ul></li>\
+            <li><a href="#">人文科学・宗教</a><ul><li><a href="/links2/human/index.html">人文科学・宗教</a></li><li><a href="/links2/human/index_history.html">歴史</a></li><li><a href="/links2/human/index_psychology.html">心理学</a></li><li><a href="/links2/human/index_religion.html">宗教</a></li><li><a href="/links2/human/index_literature.html">文学</a></li></ul></li>\
+            <li><a href="#">外国・言語</a><ul><li><a href="/links2/country/index.html">外国・言語</a></li><li><a href="/links2/country/index_ukus.html">アメリカ・イギリス・英語</a></li><li><a href="/links2/country/index_germany.html">ドイツ</a></li><li><a href="/links2/country/index_france.html">フランス</a></li></ul></li>\
+      </ul>\
+    </li>\
+    <li><a href="#">芸術</a>\
+      <ul>\
+            <li><a href="#">美術・デザイン</a><ul><li><a href="/links2/art/index.html">美術・デザイン</a></li><li><a href="/links2/art/index_designsofts.html">デザインソフトウェア</a></li></ul></li>\
+            <li><a href="/links2/music/index.html">音楽</a></li>\
+            <!--<li><a href="/links2/culture/index.html">文化・生活・スポーツ</a></li>-->\
+      </ul>\
+    </li>\
+    <!--</ul>\
+    </li>-->\
+    <li><a href="#">作品・絵</a>\
+      <ul>\
+        <li><a href="/subpages/gallery.html">イラスト・作品ギャラリー</a></li>\
+        <li><a href="/subpages/images.html">似顔絵・イメージ</a></li>\
+      </ul>\
+    </li>\
+  </ul>\
+');
+
+
+$(function() {
+  $('#main-menu').smartmenus();
+});
