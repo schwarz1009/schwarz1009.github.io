0cdad8cbb07c90f5a169cec6649c94cc7fe251a2
Assy <schwarz1009@gmail.com>
2024-12-19 16:30:46 +0900

    日記を更新。デッサンについて追記。歴史について編集。日記へのリンクを追加。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -514,6 +514,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2024.12.16.html">2024-12-16</a></li>\
 <li><a href="2024.12.17.html">2024-12-17</a></li>\
 <li><a href="2024.12.18.html">2024-12-18</a></li>\
+<li><a href="2024.12.19.html">2024-12-19</a></li>\
 </ul>\
 </div>\
 </div>\
