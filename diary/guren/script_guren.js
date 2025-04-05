47ba7db1c17af82ac022f80babb7a6b4a087f147
Assy <schwarz1009@gmail.com>
2025-04-05 16:57:19 +0900

    日記を更新。ITについて追記。音楽について追記。政治について修正。日記へのリンクを追加。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -648,6 +648,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2025.04.02.html">2025-04-02</a></li>\
 <li><a href="2025.04.03.html">2025-04-03</a></li>\
 <li><a href="2025.04.04.html">2025-04-04</a></li>\
+<li><a href="2025.04.05.html">2025-04-05</a></li>\
 </ul>\
 </div>\
 </div>\
