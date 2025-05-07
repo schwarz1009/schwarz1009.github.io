2606718de35e7522439bd585e46f6827874c95d9
Assy <schwarz1009@gmail.com>
2025-05-07 16:44:37 +0900

    日記を更新。過去の日記を編集。YouTubeについて追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -684,6 +684,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2025.05.04.html">2025-05-04</a></li>\
 <li><a href="2025.05.05.html">2025-05-05</a></li>\
 <li><a href="2025.05.06.html">2025-05-06</a></li>\
+<li><a href="2025.05.07.html">2025-05-07</a></li>\
 </ul>\
 </div>\
 </div>\
