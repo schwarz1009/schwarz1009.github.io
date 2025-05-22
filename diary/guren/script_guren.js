1a1187d15e4f87301b9429473e6bf49bf3b812b2
Assy <schwarz1009@gmail.com>
2025-05-22 19:02:37 +0900

    日記を更新。株式と歌手について追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -699,6 +699,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2025.05.19.html">2025-05-19</a></li>\
 <li><a href="2025.05.20.html">2025-05-20</a></li>\
 <li><a href="2025.05.21.html">2025-05-21</a></li>\
+<li><a href="2025.05.22.html">2025-05-22</a></li>\
 </ul>\
 </div>\
 </div>\
