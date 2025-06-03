3398ac391b1db5deffea0fad509f0c3d89df0a95
Assy <schwarz1009@gmail.com>
2025-06-03 17:04:38 +0900

    日記を更新。ゲームと歌手と医学とスケジュールについて追記。日記へのリンクを追加。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -716,6 +716,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <div id="tab_2025_06" style="display: none;">\
 <ul>\
 <li><a href="2025.06.01.html">2025-06-01</a></li>\
+<li><a href="2025.06.03.html">2025-06-03</a></li>\
 </ul>\
 </div>\
 </div>\
