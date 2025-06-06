f8984f311efc69e0cd1c44baa514a7a060b68232
Assy <schwarz1009@gmail.com>
2025-06-07 00:03:52 +0900

    色について追記・編集。

--- diary/elika/script_header.js
+++ diary/elika/script_header.js
@@ -1,6 +1,6 @@
 let div_page_menu = document.getElementsByClassName('page');
 div_page_menu[0].insertAdjacentHTML('beforebegin', '\
-<div class="article"><p class="header_text">わたしの名は、エリカ・グレーテル・フローレンス。この世界の創造主、天軍大創主エリカである。<br>好きな歌手は、家入レオ、酸欠少女さユり、鈴木愛理、miwa、AliA、MY FIRST STORY、BiSH、ReoNa、藍井エイル、Ado、ウォルピスカーター、上田桃夏、こぴ、TOMOO。特別好きなのは上田桃夏。人間として好きなのはこぴ。<br>彼女たちは、「インターネット仮想中学校」の同窓生です。僕の親友はレオちゃんで、僕の分身はさユりちゃんです。<br>あなたもこのクラスメイトの一員です。なぜなら、わたしたちはインターネットの「一億人のクラスメイト」だからです。</p></div>\
+<div class="article"><p class="header_text">わたしの名は、桃色の少女、エリカ・グレーテル・フローレンス。この世界の創造主、天軍大創主エリカである。<br>好きな歌手は、家入レオ、酸欠少女さユり、鈴木愛理、miwa、AliA、MY FIRST STORY、BiSH、ReoNa、藍井エイル、Ado、ウォルピスカーター、上田桃夏、こぴ、TOMOO。特別好きなのは上田桃夏。人間として好きなのはこぴ。<br>彼女たちは、「インターネット仮想中学校」の同窓生です。僕の親友はレオちゃんで、僕の分身はさユりちゃんです。<br>あなたもこのクラスメイトの一員です。なぜなら、わたしたちはインターネットの「一億人のクラスメイト」だからです。</p></div>\
 <div class="article"><h2>メニュー</h2><div class="section">\
 <h3>目次</h3>\
 <ul class="defaultlist list6 list_elika">\
