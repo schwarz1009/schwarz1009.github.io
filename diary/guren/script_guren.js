8bb635b4d029a04eef74a1aec7eecf1e258f24f7
Assy <schwarz1009@gmail.com>
2025-07-18 17:03:35 +0900

    日記を更新。歌手について追記。日記へのリンクを追加。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -766,6 +766,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2025.07.15.html">2025-07-15</a></li>\
 <li><a href="2025.07.16.html">2025-07-16</a></li>\
 <li><a href="2025.07.17.html">2025-07-17</a></li>\
+<li><a href="2025.07.18.html">2025-07-18</a></li>\
 </ul>\
 </div>\
 </div>\
