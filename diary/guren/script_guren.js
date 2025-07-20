999ce5be36ec44101348b9c9b1e9426d972fc3db
Assy <schwarz1009@gmail.com>
2025-07-20 17:24:55 +0900

    日記を更新。銀行について追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -768,6 +768,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2025.07.17.html">2025-07-17</a></li>\
 <li><a href="2025.07.18.html">2025-07-18</a></li>\
 <li><a href="2025.07.19.html">2025-07-19</a></li>\
+<li><a href="2025.07.20.html">2025-07-20</a></li>\
 </ul>\
 </div>\
 </div>\
