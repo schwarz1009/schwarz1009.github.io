8a0e0283492ae2fb4198c3ff090c2a63107b2501
Assy <schwarz1009@gmail.com>
2026-01-12 15:29:13 +0900

    日記を更新。過去の日記を編集。歌手について追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -943,6 +943,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2026.01.08.html">2026-01-08</a></li>\
 <li><a href="2026.01.10.html">2026-01-10</a></li>\
 <li><a href="2026.01.11.html">2026-01-11</a></li>\
+<li><a href="2026.01.12.html">2026-01-12</a></li>\
 </ul>\
 </div>\
 </div>\
