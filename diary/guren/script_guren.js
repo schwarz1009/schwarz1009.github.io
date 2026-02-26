fb617a65f9dd5bd3f638c1286e1625a6c7788dcb
Assy <schwarz1009@gmail.com>
2026-02-26 15:25:58 +0900

    日記を更新。トップページを編集。PHPとツイッターについて追記・編集。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -981,6 +981,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <li><a href="2026.02.19.html">2026-02-19</a></li>\
 <li><a href="2026.02.20.html">2026-02-20</a></li>\
 <li><a href="2026.02.22.html">2026-02-22</a></li>\
+<li><a href="2026.02.26.html">2026-02-26</a></li>\
 </ul>\
 </div>\
 </div>\
