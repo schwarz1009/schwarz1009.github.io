8ec7ef401586123165992dadfef3d8486f5c973d
Assy <schwarz1009@gmail.com>
2026-05-04 15:14:11 +0900

    日記を更新。細かい修正。ツイッターと植物について追記。

--- diary/guren/script_guren.js
+++ diary/guren/script_guren.js
@@ -1056,6 +1056,7 @@ div_guren_script.insertAdjacentHTML('beforebegin', '\
 <div id="tab_2026_05" style="display: none;">\
 <ul>\
 <li><a href="2026.05.03.html">2026-05-03</a></li>\
+<li><a href="2026.05.04.html">2026-05-04</a></li>\
 </ul>\
 </div>\
 </div>\
